30c3833ee198d8fe21b25ebb0f99a939b1d7a336
Arnold Shepitko <Arnold.shepitko@hochschule-stralsund.de>
2021-12-11 14:56:01 +0100

    Add to AppCenter and build scripts

--- app/src/main/java/com/ynk/todolist/Activitys/MainActivity.java
+++ app/src/main/java/com/ynk/todolist/Activitys/MainActivity.java
@@ -22,6 +22,11 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
+
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
 import com.ynk.todolist.Fragments.FragmentTodoList;
 import com.ynk.todolist.Listeners.MainListener;
 import com.ynk.todolist.Model.User;
@@ -38,6 +43,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        AppCenter.start(getApplication(), "3f1949c0-56da-4de9-9830-666d563e22a4",
+                Analytics.class, Crashes.class);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
