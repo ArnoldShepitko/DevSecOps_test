585ead65a15284eee389c9bb6a465ef83812c6fd
Arnold Shepitko <Arnold.shepitko@hochschule-stralsund.de>
2021-12-11 16:40:35 +0100

    update Scripts 2

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -4,5 +4,5 @@ echo "Found Unit test projects:"
 find $APPCENTER_SOURCE_DIRECTORY -regex '.*UnitTests.*\.java' exec echo {} \;
 echo
 echo "Run Unit test projects:"
-find $APPCENTER_SOURCE_DIRECTORY -regex '.*UnitTests.*\.java' |  | xargs dotnet
+find $APPCENTER_SOURCE_DIRECTORY -regex '.*UnitTests.*\.java' | xargs dotnet
 test;
